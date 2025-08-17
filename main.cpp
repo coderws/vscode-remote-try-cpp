edc14974cb22f8ebb83d39d558bdd98ebc86c9f2
coderws <wscoder@outlook.com>
2025-08-17 12:02:03 +0000

    test 内存安全

--- main.cpp
+++ main.cpp
@@ -4,10 +4,16 @@
 using namespace std;
 
 int main() {
+    int a[3] = {1,2,3};
+    // 内存不安全的代码
+    printf("%d\n", a[10]);
+    printf("%d\n", a[0]);
+
     cout << "Hello World\n";
     cout << "Input: ";
     string data;
     getline(cin, data);
     cout << "Output: " << data << "\n\n";
+
     return 0;
 }
